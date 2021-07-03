0d2fbf48d73e036f683743e493714498d9b5a5b6
mississilia.chabane <mississilia.chabane@gmail.com>
2021-07-03 17:34:48 +0200

    tmp

--- src/main/java/fr/esgi/rent_car/RentCarApplication.java
+++ src/main/java/fr/esgi/rent_car/RentCarApplication.java
@@ -2,6 +2,7 @@ package fr.esgi.rent_car;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class RentCarApplication {
